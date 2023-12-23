c6d48ea64d8557b6eb85838dec476bf6f757feab
chenboan <andypig9@gmail.com>
2023-12-23 14:11:42 +0800

    log env

--- test_hello_world.py
+++ test_hello_world.py
@@ -1,7 +1,12 @@
 import logging
 import os
 
+from dotenv import load_dotenv
+
 
 def test_hello_world():
-    env_file = os.environ.keys()
-    logging.info(env_file)
+    if 'ENV' in os.environ:
+        env_file = os.environ['ENV']
+        load_dotenv(env_file)
+    else:
+        load_dotenv()
